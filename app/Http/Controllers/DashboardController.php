d993e544035b418b0cbf70c1ce8908f138ee0665
KaffahRizal <kaffahrizal@gmail.com>
2024-07-11 09:27:28 +0700

    info dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Faskes;
+use App\Models\KabKota;
+use App\Models\Provinsi;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        return view('admin.contents.dashboard.index');
+    public function show()
+    {
+        // ambil semua data dri tabel faskes
+        $totalfaskes = Faskes::count();
+        $totalprovinsi = Provinsi::count();
+        $totalkabkota = KabKota::count();
+
+        // kirim ke view
+        return view('admin.contents.dashboard.index', [
+            'totalfaskes' => $totalfaskes,
+            'totalprovinsi' => $totalprovinsi,
+            'totalkabkota' => $totalkabkota
+
+        ]);
     }
 }
